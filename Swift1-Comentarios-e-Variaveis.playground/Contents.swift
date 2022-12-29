0e88afc9b985a1c8df5375ea710830fa4511896d
vloss <viniciusvianaloss@gmail.com>
2022-12-28 21:32:17 -0300

    Introdução a variaveis.

--- Swift1-Comentarios-e-Variaveis.playground/Contents.swift
+++ Swift1-Comentarios-e-Variaveis.playground/Contents.swift
@@ -1,3 +1,21 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// Variaveis = Mutable
+var str = "Olá Mundo!"
+print(str)
+
+var name = "Vinicius"
+print(name)
+
+var age = 31
+print(age)
+
+var 🐶 = "Puma"
+print(🐶)
+
+
+// Constantes = Unmutable
+let pi = 3.1415926
+let mothsInYear = 12
+
+let Pi = 3.1415
