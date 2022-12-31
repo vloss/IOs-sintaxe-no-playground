0b4f8be33b47518d72a0353b052b02a4e3167942
vloss <viniciusvianaloss@gmail.com>
2022-12-31 18:49:51 -0300

    Finalizando Dicionários.

--- Swift1-Dicionários.playground/Contents.swift
+++ Swift1-Dicionários.playground/Contents.swift
@@ -1,3 +1,49 @@
-import UIKit
+var states = [
+    "AC" : "Acre",
+    "AL" : "Alagoas",
+    "AP" : "Amapá",
+    "AM" : "Amazonas",
+    "BA" : "Bahia",
+    "CE" : "Ceará",
+    "DF" : "Distrito Federal",
+    "ES" : "Espírito Santo"
+]
 
-var greeting = "Hello, playground"
+// Dicionario com tipo definido inicializando em zero
+var studentGrade: [String: Double] = [:]
+
+
+states["GO"] = "Goiaz"
+
+//states["GO"] = "Goiás"
+
+
+let outGO = states.updateValue("Goiás", forKey: "GO")
+
+print(states)
+print(outGO)
+
+
+let myState = states["PA"]
+
+
+if let state = myState{
+    print(state)
+}
+
+states["GO"] = nil
+print(states)
+
+let uf = states.removeValue(forKey: "DF")
+
+print(states)
+
+states.count
+
+
+let arrayStates = Array(states.keys)
+arrayStates[0]
+
+
+let arrayValues = Array(states.values)
+arrayValues[0]
