305db29eb99db941217d5d16848817b679eb1cbc
vloss <viniciusvianaloss@gmail.com>
2023-01-04 20:40:03 -0300

    Continuação de Closures

--- Swift1-Closures.playground/Contents.swift
+++ Swift1-Closures.playground/Contents.swift
@@ -1,1 +1,26 @@
+// Closures
 
+func sum (_ a:Int, b: Int) -> Int{
+    return a + b
+}
+
+func subtract (_ a:Int, b: Int) -> Int{
+    return a - b
+}
+
+typealias Op = (Int, Int) -> Int
+
+func calculate(_ a:Int, _ b: Int, operation: Op) -> Int {
+    return operation(a, b)
+}
+
+
+let result = calculate(1,2, operation: sum)
+
+
+calculate(10,20, operation: {(num1: Int, num2: Int) -> Int in
+    return num1 * num2
+})
+
+
+// Forma simplificada.
