ab7274dce473869840a92d6830585d55eed63df2
vloss <viniciusvianaloss@gmail.com>
2023-01-04 18:45:16 -0300

    Fazendo uma função receber outra função como parametro, e também fazer uma função retornar outra função atribuindo a uma variável a  função.

--- Swift1-Funçoes-Parte-02.playground/Contents.swift
+++ Swift1-Funçoes-Parte-02.playground/Contents.swift
@@ -1,3 +1,49 @@
 // Funções parte 02
 
+// First Class Citizen
 
+func sum(_ a: Int, _ b:Int) -> Int {
+    return a + b
+}
+
+
+func subtract(_ a: Int, _ b:Int) -> Int {
+    return a - b
+}
+
+
+func divide(_ a: Int, _ b:Int) -> Int {
+    return a / b
+}
+
+
+func multiply(_ a: Int, _ b:Int) -> Int {
+    return a * b
+}
+
+typealias Op = (Int, Int) -> Int
+
+//Passando como parametro uma variavel do tipo operacao que vem a ser uma funçao
+func applyOperation(_ a: Int, _ b:Int, operation: Op) -> Int {
+    return operation(a, b)
+}
+
+let result = applyOperation(40, 20, operation: sum)
+
+// Retornando uma funcao
+func getOperation (_ operation: String) -> Op {
+    switch operation {
+        case "soma":
+            return sum
+        case "subtração":
+            return subtract
+        case "divisão":
+            return divide
+        default:
+            return multiply
+    }
+}
+
+
+var operation = getOperation("divisão")
+operation(9,3)
