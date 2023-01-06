9e3d7ffed29a0a4d673eaa61872f2ea391e6b839
vloss <viniciusvianaloss@gmail.com>
2023-01-05 23:26:30 -0300

    Finalizando tratamento de erros.

--- Swift1-Tratamento-de-Erros.playground/Contents.swift
+++ Swift1-Tratamento-de-Erros.playground/Contents.swift
@@ -1,3 +1,43 @@
-import UIKit
+// Tratamento de Erros
 
-var greeting = "Hello, playground"
+enum LoginError: Error {
+    case wrongLogin
+    case wrongPassword
+    case noInternetConnection
+}
+
+func login(userName: String, password: String) throws -> Bool {
+    if checkInternetConnection(){
+        if userName != "vini"{
+            throw LoginError.wrongLogin
+        }
+        if password != "123456" {
+            throw LoginError.wrongPassword
+        }
+        return true
+
+    } else {
+        throw LoginError.noInternetConnection
+    }
+}
+
+func checkInternetConnection() -> Bool {
+    return false
+}
+
+do {
+    let loginResult = try login(userName: "vini", password: "123456")
+} catch LoginError.wrongLogin {
+    print("Nome do Usuário inválido!")
+} catch LoginError.wrongPassword {
+    print("Senha do Usuário inválida!")
+} catch LoginError.noInternetConnection {
+    print("Sem conexão com Internet!")
+}
+
+// ? Caso não seja necessário tratar o erro irá retornar Nil
+let loginResult = try? login(userName: "vini", password: "123456")
+
+// ! Caso não seja necessário tratar o erro, porém se der erro irá travar
+// let loginResult = try! login(userName: "vini", password: "123456")
+
